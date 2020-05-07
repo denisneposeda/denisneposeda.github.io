815b7cf8a9e0e859b05a5634b1099e8e4d685543
denisneposeda <44367689+denisneposeda@users.noreply.github.com>
2020-05-07 13:08:46 +0300

    Update Armis

---
+++ armis/js/common.js
@@ -0,0 +1,40 @@
+$(function() {
+
+	$('.lazy').Lazy({
+      scrollDirection: 'vertical',
+      effect: 'fadeIn',
+      visibleOnly: true,
+      onError: function(element) {
+          console.log('error loading ' + element.data('src'));
+      }
+    })
+
+    $('.hamburger').click(function(e){
+        e.preventDefault();
+        $(this).toggleClass('is-active');
+        $('html').toggleClass('is-menu');
+    })
+
+    var promoSlider = $('.promo-slider .owl-carousel').owlCarousel({
+        loop:false,
+        margin:0,
+        nav:false,
+        dots:false,
+        mouseDrag:false,
+        touchDrag:false,
+        smartSpeed: 750,
+        animateOut: 'fadeOut',
+        items: 1
+    })
+
+    $('.slider-control_link').eq(0).addClass('is-active');
+    $('.slider-control_link').click(function(e){
+        e.preventDefault();
+        if ( $(this).not('.is-active') ) {
+            $(this).addClass('is-active').siblings('.is-active').removeClass('is-active');
+            var index = $(this).index();
+            promoSlider.trigger('to.owl.carousel', index);
+        }
+    })
+
+});
