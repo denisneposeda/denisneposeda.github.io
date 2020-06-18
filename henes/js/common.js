2f07d3570172f2ae42419977de6b05e5f5c78a2e
denisneposeda <44367689+denisneposeda@users.noreply.github.com>
2020-06-18 21:12:02 +0300

    Denis
    Update Henes

---
+++ henes/js/common.js
@@ -0,0 +1,59 @@
+$(function() {
+
+	$('.lazy').Lazy({
+      scrollDirection: 'vertical',
+      effect: 'fadeIn',
+      visibleOnly: true,
+      onError: function(element) {
+          console.log('error loading ' + element.data('src'));
+      }
+    })
+
+    $('.hamburger').click(function(e){
+        e.preventDefault();
+        $(this).toggleClass('is-active');
+        $('html').toggleClass('is-menu');
+    })
+
+    var catalogCarousel = $('.catalog-carousel').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:false,
+        dots:false,
+        mouseDrag: false,
+        touchDrag: false,
+        smartSpeed: 750,
+        items: 1
+    })
+
+    $('.catalog-carousel_btn').eq(0).addClass('is-active');
+    $('.catalog-carousel_btn').click(function(e){
+        e.preventDefault();
+        if (  $(this).not('.is-active') ) {
+            $(this).addClass('is-active').parent().siblings().children('.is-active').removeClass('is-active');
+            var index = $(this).parent().index();
+            catalogCarousel.trigger('to.owl.carousel', index);
+        }
+    })
+
+    $('.photo-carousel').owlCarousel({
+        loop:false,
+        margin:30,
+        nav:true,
+        dots:false,
+        smartSpeed: 750,
+        slideBy: 1,
+        responsive:{
+            0:{
+                items:1
+            },
+            415:{
+                items:2
+            },
+            768:{
+                items:3
+            }
+        }
+    })
+
+});
